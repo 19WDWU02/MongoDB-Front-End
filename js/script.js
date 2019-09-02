2607d8b57df740bda408021d94e89b62eda1081f
Richard Hpa <richard.hpa@bun2-206-05.local>
2019-09-03 10:41:35 +1200

    added more comments throughout the code

--- js/script.js
+++ js/script.js
@@ -11,7 +11,9 @@ $.ajax({
   success:function(keys){
     serverURL = keys['SERVER_URL'];
     serverPort = keys['SERVER_PORT'];
+    // create a variable which will be our servers url (it will be easier than having to write out the two variables later on)
     url = `${keys['SERVER_URL']}:${keys['SERVER_PORT']}`;
+    // Get all the products
     getProductsData();
   },
   error: function(){
@@ -21,29 +23,38 @@ $.ajax({
 
 // Get all the products
 getProductsData = () => {
+    // run an ajax request to the route to get all the products
     $.ajax({
         // url: `${serverURL}:${serverPort}/allProducts`,
         url: `${url}/allProducts`,
         type: 'GET',
         dataType: 'json',
         success:function(data){
+            // because we run this function multiple times now, we need to empty the product list each time we call it
             $('#productList').empty();
+            // Loop over all the items/products which get back from the database
             for (var i = 0; i < data.length; i++) {
+                // Create a variable called product which will hold our template string for our product
                 let product = `
                     <li
                         class="list-group-item d-flex justify-content-between align-items-center productItem"
                         data-id="${data[i]._id}"
                     >
                         <span class="productName">${data[i].name}</span>`;
+
+                        // We only want to see the edit and remove buttons when a user is logged in.
+                        // So we have closed the string above and written an if statement to only add on the buttons
+                        // if someone is logged on.
                         if(sessionStorage['userName']){
                             product += `<div>
                                             <button class="btn btn-info editBtn">Edit</button>
                                             <button class="btn btn-danger removeBtn">Remove</button>
                                         </div>`;
                         }
-                    product += `</li>
-                `;
+                    // either way we have to close the li so we add the closing li at the end.
+                product += `</li>`;
 
+                // Once we have created our product variable with all the data/html, we append it to the productList ul
                 $('#productList').append(product);
             }
         },
@@ -56,16 +67,27 @@ getProductsData = () => {
 
 //Add or Edit a product
 $('#addProductButton').click(function(){
+    // prevent the default from happening
     event.preventDefault();
+    // Get the values for the product name and price
     let productName = $('#productName').val();
     let productPrice = $('#productPrice').val();
+    // Check to see if there is a value in the product name
     if(productName.length === 0){
         console.log('please enter a products name');
     } else if(productPrice.length === 0){
+        // Check to see if there is a value for the product price
         console.log('please enter a products price');
     } else {
+        // only when the there is a product name or price do we want to send some data.
+
+        // We check to see if there if we are editing a product or creating a new one
+        // by default editing will be false, but will change to true when we have clicked edit on a product
         if(editing === true){
+            // get the id of the product we want to edit
+            // this id is saved in a hidden input field
             const id = $('#productID').val();
+            // run an ajax request to our edit/patch route
             $.ajax({
                 url: `${url}/product/${id}`,
                 type: 'PATCH',
@@ -74,16 +96,29 @@ $('#addProductButton').click(function(){
                     price: productPrice
                 },
                 success:function(result){
+                    // if we successfully edit a product
+
+                    // set the productName, productPrice and productID input fields to null
                     $('#productName').val(null);
                     $('#productPrice').val(null);
                     $('#productID').val(null);
+                    // change the button back to add new product and remove the warning class
                     $('#addProductButton').text('Add New Product').removeClass('btn-warning');
+                    // change heading back to add new product
                     $('#heading').text('Add New Product');
+                    // now that editing has finsihed, change the editing variable back to false
                     editing = false;
+                    // We need to change the value of the single product we just edited.
+                    // Get all of the products, all of the li's should have a class of productItem
                     const allProducts = $('.productItem');
+                    // Loop over each of those items
                     allProducts.each(function(){
+                        // check to see if the data-id value of that li matches the id which we are editing
                         if($(this).data('id') === id){
+                            // find the span which holds the name of the product and change it
                             $(this).find('.productName').text(productName);
+                            // stop the each function
+                            return false;
                         }
                     });
                 },
@@ -93,6 +128,9 @@ $('#addProductButton').click(function(){
                 }
             })
         } else {
+            // This section will only run when we aren't editing, but adding a new product
+            // send an ajax request to the create/post route.
+            // we need to add in the data which we are wanting to send
             $.ajax({
                 url: `${url}/product`,
                 type: 'POST',
@@ -101,8 +139,11 @@ $('#addProductButton').click(function(){
                     price: productPrice
                 },
                 success:function(result){
+                    // once we have successfully added a product to the database.
+                    // remove the input values from productName and productPrice to null (wipe the input fields)
                     $('#productName').val(null);
                     $('#productPrice').val(null);
+                    // append a new product to the bottom of the productList
                     $('#productList').append(`
                         <li class="list-group-item d-flex justify-content-between align-items-center productItem">
                             <span class="productName">${result.name}</span>
@@ -125,18 +166,27 @@ $('#addProductButton').click(function(){
 
 // Edit button to fill the form with exisiting product
 $('#productList').on('click', '.editBtn', function() {
+    // prevent the default from happening
     event.preventDefault();
+    // Get the ID of the product we want to edit.
+    // We have saved the id into data-id to the parent li of the button
     const id = $(this).parent().parent().data('id');
+    // send an ajax request to our route for getting single product
     $.ajax({
         url: `${url}/product/${id}`,
         type: 'get',
         dataType: 'json',
         success:function(product){
+            // replace the input fields with the name and price from the database
             $('#productName').val(product['name']);
             $('#productPrice').val(product['price']);
+            // we have a hidden input field which we need to give it the value of the products id
             $('#productID').val(product['_id']);
+            // Change the buttons text to edit and add the warning class
             $('#addProductButton').text('Edit Product').addClass('btn-warning');
+            // Change the heading text
             $('#heading').text('Edit Product');
+            // set the global variable of editing to true
             editing = true;
         },
         error:function(err){
@@ -148,14 +198,21 @@ $('#productList').on('click', '.editBtn', function() {
 
 // Remove a product
 $('#productList').on('click', '.removeBtn', function(){
+    // prevent the default from happening
     event.preventDefault();
+    // Get the ID of the product we want to edit.
+    // We have saved the id into data-id to the parent li of the button
     const id = $(this).parent().parent().data('id');
+    // Get the li
     const li = $(this).parent().parent();
+    // Run the ajax request to the delete route
     $.ajax({
       url: `${url}/product/${id}`,
       type: 'DELETE',
       success:function(result){
-        li.remove();
+          // success will happen when we have deleted a product from the database
+          // remove the li which the button is related to
+          li.remove();
       },
       error:function(err) {
         console.log(err);
@@ -164,18 +221,28 @@ $('#productList').on('click', '.removeBtn', function(){
     })
 });
 
+// Click on the Login Tab
 $('#loginTabBtn').click(function(){
+    // prevent the default from happening
     event.preventDefault();
+    // Remove the active class from all of the tabs
     $('.nav-link').removeClass('active');
+    // Add the active class to the current tab
     $(this).addClass('active');
+    // Show/Hide the login and register forms
     $('#loginForm').show();
     $('#registerForm').hide();
 });
 
+// Click on the Register Tab
 $('#registerTabBtn').click(function(){
+    // prevent the default from happening
     event.preventDefault();
+    // Remove the active class from all of the tabs
     $('.nav-link').removeClass('active');
+    // Add the active class to the current tab
     $(this).addClass('active');
+    // Show/Hide the login and register forms
     $('#loginForm').hide();
     $('#registerForm').removeClass('d-none').show();
 
@@ -215,6 +282,8 @@ $('#registerForm').submit(function(){
                 password: password
             },
             success:function(result){
+                // We are logging the result of the register form.
+                // We could either log the user in automatically, or switch to the login tab
                 console.log(result);
             },
             error:function(err){
@@ -269,7 +338,9 @@ $('#loginForm').submit(function(){
                     sessionStorage.setItem('userID', result['_id']);
                     sessionStorage.setItem('userName', result['username']);
                     sessionStorage.setItem('userEmail', result['email']);
+                    // Reload all the products
                     getProductsData();
+                    // Hide and show the relevant content
                     $('#authForm').modal('hide');
                     $('#loginBtn').hide();
                     $('#logoutBtn').removeClass('d-none');
@@ -284,10 +355,13 @@ $('#loginForm').submit(function(){
     }
 });
 
-
+//Logout Button
 $('#logoutBtn').click(function(){
+    // Clear the session storage to remove the user
     sessionStorage.clear();
+    // Reload all the products
     getProductsData();
+    // Hide and show the relevant content
     $('#loginBtn').show();
     $('#logoutBtn').addClass('d-none');
     $('#addProductSection').addClass('d-none');
